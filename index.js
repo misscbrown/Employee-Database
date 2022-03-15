a2af380f516297a1b50be7cf939e5c765f5045ca
cee breezy <djmisscbrown@hotmail.com>
2022-03-15 10:06:40 +0000

    Added dependencies to js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const inquirer = require('inquirer'),
+// dependencies
+const inquirer = require("inquirer");
+let database = require("./db");
+let consoleTable = require("console.table");
+
+const db = new database({
+    host: "localhost",
+    port: 3000,
+    user: "root",
+    password: "password",
+    database: "cms"
+});
+
