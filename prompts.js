aa91a1a699dca7199b7ba4174177a7aeef1209ed
cee breezy <djmisscbrown@hotmail.com>
2022-03-16 12:58:28 +0000

    Added function and exit option

--- prompts.js
+++ prompts.js
@@ -3,6 +3,7 @@
 const inquirer = require("inquirer");
 let Database = require("./database.js");
 let consoleTable = require("console.table");
+//const { DEC8_BIN } = require("mysql/lib/protocol/constants/charsets");
 
 // Questions for the user to answer
 function mainQuestions() {
@@ -99,3 +100,104 @@ function getDepartmentInfo() {
         }
     ])
 }
+
+function getUpdateEmployeeRoleInfo() {
+    const employees = getEmployeeNames();
+    const roles = getRoles();
+    return inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'Select the employee you would like to update',
+            name: 'employeeName',
+            choices: [
+                ...employees
+            ]
+        },
+        {
+            type: 'list',
+            message: 'What is the employees new role?',
+            name: 'role',
+            choices: [
+                ...roles
+            ]
+        }
+    ])
+}
+
+async function main() {
+    let exitLoop = false;
+    while(!exitLoop) {
+        const prompt = await mainQuestions();
+
+        switch(prompt.action) {
+            case 'Add department': {
+                const newDepartmentName = await getDepartmentInfo();
+                await addDepartment(newDepartmentName);
+                break;
+            }
+            
+            case 'Add employee': {
+                const newEmployee = await getAddEmployeeInfo();
+                console.log('add employee' + newEmployee);
+                await getAddEmployeeInfo(newEmployee);
+                break;
+            }
+
+            case 'Add role': {
+                const newRole = await getRoleInfo()
+                await addRole(newRole);
+                break;
+            }
+
+            case 'Remove employee': {
+                const employee = await getRemoveEmployeeInfo();
+                await removeEmployee(employee);
+                break;
+            }
+
+            case 'Update employee role': {
+                const employee = await getUpdateEmployeeRoleInfo();
+                await updateEmployeeRole(employee);
+                break; 
+            }
+
+            case 'View all departments': {
+                await viewAllDepartments();
+                break;
+            }
+
+            case 'View all employees': {
+                await viewAllEmployees();
+                break;
+            }
+
+            case 'View all employees by department': {
+                await viewAllEmployeesByDepartment();
+                break;
+            }
+
+            case 'View all roles': {
+                await viewAllRoles();
+                break;
+            }
+
+            case 'Exit': {
+                exitLoop = true;
+                process.exit(0);
+                return;
+            }
+
+            default:
+                console.log(`Hey! You shouldn't get here! Action ${prompt.action}`);
+        }
+    }
+}
+
+// Closes the database connection
+process.on('exit', async function(code) {
+    await db.close();
+    return console.log(`About to exit with code ${code}`);
+});
+
+main();
